bd28ed740bb021d1e0c0ef6f276fdacdd666242b
Aleksandar Ivanov <aivanov92@gmail.com>
2013-10-27 13:53:24 +0200

    Add oauth button and dummy dashboard page

--- sample-client/lib/sample_client.pm
+++ sample-client/lib/sample_client.pm
@@ -1,10 +1,27 @@
 package sample_client;
+
+use strict;
+use warnings;
+
 use Dancer ':syntax';
+use URI;
+use URI::QueryParam;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+    my $oauth = config->{oauth};
+    my $susi_oauth_url = URI->new($oauth->{url});
+
+    $susi_oauth_url->query_param(response_type => $oauth->{response_type});
+    $susi_oauth_url->query_param(client_id => $oauth->{client_id});
+    $susi_oauth_url->query_param(redirect_uri => $oauth->{redirect_uri});
+
+    template 'index', {susi_oauth_url => $susi_oauth_url->as_string};
+};
+
+get '/dashboard' => sub {
+    template 'dashboard';
 };
 
 true;
